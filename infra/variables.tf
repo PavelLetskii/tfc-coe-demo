3dd2b61ff4ed146fbc9ae66fbb53d663e6fc2bc4
Pavel Letskii <pavel.letskii@nordcloud.com>
2022-10-03 12:46:27 +0300

    Add infra code

---
+++ infra/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+variable "profile" {
+  description = "AWS profile"
+  type        = string
+  default     = "sandbox"
+}
