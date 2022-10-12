bca4dd5fc96dd933d2b8723eff8b2ed8ce0512c8
Pavel Letskii <pavel.letskii@nordcloud.com>
2022-10-12 16:20:31 +0300

    Monitoring module. First use. Fix 3

--- modules/monitor/vars.tf
+++ modules/monitor/vars.tf
@@ -1,17 +1,17 @@
-variable "vault_address" {
-  type        = string
-  description = "Address to the Vault instance"
-}
+# variable "vault_address" {
+#   type        = string
+#   description = "Address to the Vault instance"
+# }
 
-variable "vault_storage_path" {
-  type        = string
-  description = "Path to the config storage in Vault"
-}
+# variable "vault_storage_path" {
+#   type        = string
+#   description = "Path to the config storage in Vault"
+# }
 
-variable "vault_token" {
-  type        = string
-  description = "Access token to the Vault secret storage"
-}
+# variable "vault_token" {
+#   type        = string
+#   description = "Access token to the Vault secret storage"
+# }
 
 variable "customer_name" {
   type        = string
